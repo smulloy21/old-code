263fada4cc427d24484fce736b137b8d47bd8c27
Guest User <student@epicodus.com>
2015-09-28 15:59:04 -0700

    add add-category component and categories

---
+++ app/components/add-category.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    saveCategory() {
+      var params = {
+        name: this.get('name'),
+        subcategories: [],
+        image: this.get('image')
+      }
+      this.sendAction('saveCategory', params);
+    }
+  }
+
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('category');
+  },
+
+  actions: {
+    saveCategory(params) {
+      var newCategory = this.store.createRecord('category', params);
+      newCategory.save();
+      this.transitionTo('index');
+    }
+
   }
+
+
 });
