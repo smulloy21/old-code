9b02ecda1714a106981f2c5ec9fb8e101d6e418b
Shannon Mulloy <student@epicodus.com>
2015-09-29 11:12:30 -0700

    add ability to edit and delete listings

--- app/components/listing-details.js
+++ app/components/listing-details.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editMe: false,
+  actions: {
+    showEditMe() {
+      this.set('editMe', true);
+    },
+    hideEditMe() {
+      this.set('editMe', false)
+    },
+    update(listing) {
+      var params = {
+        name: this.get('name'),
+        title: this.get('title'),
+        email: this.get('email'),
+        location: this.get('location'),
+        details: this.get('details'),
+        image: this.get('image'),
+      };
+      this.set('editMe', false);
+      this.sendAction('update', listing, params)
+    },
+    delete(listing) {
+      this.set('editMe', false); 
+      this.sendAction('delete', listing)
+    }
+  }
 });
--- app/routes/listing.js
+++ app/routes/listing.js
@@ -3,5 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('listing', params.listing_id);
+  },
+  actions: {
+    update(listing, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined && params[key]!=="") {
+          listing.set(key, params[key]);
+        }
+      });
+      listing.save();
+      this.transitionTo('listing', listing.id);
+    },
+    delete(listing) {
+      var subcat = listing.subcategory;
+      listing.destroyRecord().then(function() {
+        subcat.save();
+      });
+      this.transitionTo('subcategory', subcat.id);
+    }
   }
 });
